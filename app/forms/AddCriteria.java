b410f105cb592083b05e07d79d8a87a6cafd4671
FRank-MBP <thanachot7@hotmail.com>
2015-04-19 02:28:53 +0700

    fix bug

--- app/forms/AddCriteria.java
+++ app/forms/AddCriteria.java
@@ -12,7 +12,7 @@ public class AddCriteria {
         if (name.equals("")) {
             return "Fill a criteria.";
         }
-        if (Criteria.find.where().eq("name", name.toLowerCase()).findRowCount() > 0){
+        if (Criteria.find.where().eq("name", name).findRowCount() > 0){
             return "Have this criteria in the system.";
         }
         return null;
